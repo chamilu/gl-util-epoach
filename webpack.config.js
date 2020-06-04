eb8e6b4a69d4f00c0f831b9a647a1e9e57f7c362
Chamil Udayanga <chamiludayanga28@gmail.com>
2020-06-05 02:29:43 +0800

    Added favicon style changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const Path = require("path");
+const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 const config = () => ({
     entry: { main: "./src/index.js" },
     output: {
-        path: Path.resolve(__dirname, "dist"),
+        path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js"
     },
     plugins: [
@@ -15,7 +16,15 @@ const config = () => ({
             template: "./src/index.html",
             filename: "index.html"
         }),
-        new MiniCssExtractPlugin()
+        new MiniCssExtractPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "src", "assets"),
+                    to: path.resolve(__dirname, "dist")
+                }
+            ]
+        })
     ],
     module: {
         rules: [
