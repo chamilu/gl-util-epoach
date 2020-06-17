224b86821bd0be7185de9856a78f8b3bddc16d5b
Chamil Udayanga <chamiludayanga28@gmail.com>
2020-06-17 15:21:41 +0800

    Refresh date in every minute

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import moment from "moment";
 
 const Date = () => {
-    const epoach = moment("1970-01-01").format("YYYY-MM-DD");
-    const today = moment();
-    const date = today.diff(epoach, "days");
+    const [date, setDate] = useState(0);
+
+    const getDate = () => {
+        const epoach = moment("1970-01-01").format("YYYY-MM-DD");
+        const today = moment();
+        const value = today.diff(epoach, "days");
+        setDate(value);
+    };
+
+    useEffect(() => {
+        getDate();
+        setInterval(() => {
+            getDate();
+        }, 60 * 1000);
+    }, []);
 
     return (
         <div id="date-wrapper">
