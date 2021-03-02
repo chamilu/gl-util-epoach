af72c2ef58a0825c86e2bd38e5dab245b26c319e
Chamil Udayanga <chamiludayanga28@gmail.com>
2021-03-03 01:52:54 +0800

    Added quote component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
+
 import { VIEWS } from "../utils/constants";
 
 import Date from "./Date";
 import Time from "./Time";
 import Menu from "./Menu";
+import Quotes from "./Quotes";
 import DateToEpoch from "./DateToEpoch";
 import SecondsToDate from "./SecondsToDate";
 
 const App = () => {
     const [view, setView] = useState(VIEWS.MAIN);
+
     return (
         <div id="wrapper">
             <Menu view={view} setView={setView} />
             <h1>epoch</h1>
+            <Quotes />
+            <div style={{ marginBottom: "50px" }}></div>
             <div style={{ display: view === VIEWS.MAIN ? "block" : "none" }}>
                 <Date />
                 <Time />
