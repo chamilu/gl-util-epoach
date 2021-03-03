3eaef8194764a31fd405ae9eaa90377026ec865a
Chamil Udayanga <chamiludayanga28@gmail.com>
2021-03-03 20:04:30 +0800

    Modified timestamp format

--- src/components/SecondsToDate.jsx
+++ src/components/SecondsToDate.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import moment from 'moment'
+import {TIMESTAMP_FORMAT} from '../utils/constants'
 
 function SecondsToDate() {
-    // const date = new Date(1593181260000);
     const [date, setDate] = useState("");
 
     const getDateFromSeconds = (e) => {
         const { value } = e.target;
         if (value && value.length === 13) {
-            setDate(new Date(parseInt(value, 10)));
+            const formattedDate = moment(new Date(parseInt(value, 10))).format(TIMESTAMP_FORMAT)
+            setDate(formattedDate);
         } else {
             setDate("Invalid! eg: 1593181260000");
         }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
+export const TIMESTAMP_FORMAT = "YYYY-MM-DD HH:mm:ss.SSSSSS";
+
 export const VIEWS = {
     MAIN: "main_view",
     REVERSE: "reverse_view"
